6e1a10d24328698b354b9a79d479a18b3d684dc6
Alina906745 <132589219+Alina906745@users.noreply.github.com>
2024-01-08 15:56:59 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <iostream>
+#include "work.h"
+
+int main(char* ar[], int count)
+{
+    Work sn;
+
+    sn.addnewMan("Oleg");
+    sn.addnewMan("Kostya");
+    sn.addnewMan("Misha");
+    sn.addnewMan("Vika");
+    sn.addnewMan("Lesha");
+
+    sn.delMan("Vika");
+    sn.addnewMan("Anya");
+
+    sn.addFriendship("Oleg", "Kostya");
+    sn.addFriendship("Kostya", "Misha");
+    sn.addFriendship("Misha", "Anya");
+    sn.addFriendship("Anya", "Lesha");
+
+    sn.showThreeHandshakes();
+
+    return 0;
+}
